924c4ba360faca122c6725d4b7474165b0830802
Angela Clarke <angeladoust@hotmail.com>
2022-05-18 17:38:17 +1000

    min_val and max_val functions

--- weather.py
+++ weather.py
@@ -2,8 +2,7 @@ import csv
 from datetime import datetime
 from encodings import utf_8
 import statistics
-import pandas as pd
-
+import pandas 
 
 DEGREE_SYBMOL = u"\N{DEGREE SIGN}C"
 
@@ -46,8 +45,8 @@ def convert_f_to_c(temp_in_farenheit):
     return temp_c
 print(convert_f_to_c(390))
 
+weather_data = [35,6,14,26,9,1]
 
-weather_data = [5,9,32,27,56]
 def calculate_mean(weather_data): #https://appdividend.com/2022/01/19/python-mean/
     weather_list = []
     """Calculates the mean value from a list of numbers.
@@ -73,16 +72,32 @@ def load_data_from_csv(csv_file):
         A list of lists, where each sublist is a (non-empty) line in the csv file.
     """
   
-    df = pd.read_csv(csv_file, delimiter=',')
-    list_of_rows = [list(row) for row in df.values]
-    print(list_of_rows)
-    return csv_file
+#     df = pd.read_csv(csv_file, delimiter=',')
+#     list_of_rows = [list(row) for row in df.values] I tried PANDAS and it failed so I went back to forloops
+#     for line in list_of_rows:
+#         list_of_rows = list(map(float,line[1]))
+
+#     return csv_file
+# print(load_data_from_csv("tests/data/example_one.csv"))    
+
+
+    weather_data = []
+
+    with open(csv_file, encoding="utf-8") as csv_file: 
+        file_reader = csv.reader(csv_file, delimiter = ",")
+        next(file_reader)
+        for line in file_reader: 
+            if line:
+                weather_data.append([line[0],float(line[1]),float((line[2]))])      
+    return weather_data
 
 print(load_data_from_csv("tests/data/example_one.csv"))    
 
 
+weather_data = [49, 57, 56, 55, 53, 49]
 
-def find_min(weather_data):
+
+def find_min(weather_data): #https://www.youtube.com/watch?v=fdwyp1xvD_I
     """Calculates the minimum value in a list of numbers.
 
     Args:
@@ -90,7 +105,38 @@ def find_min(weather_data):
     Returns:
         The minium value and it's position in the list.
     """
-    pass
+    # for weather in weather_data:
+    #     weather_list = []
+    #     weather = float(weather)
+    #     weather_list.append(weather) 
+    #     list_min = min(weather_data)
+    #     min_index = weather_data.index(list_min)
+    #     find_min = print(f"{list_min}, {min_index}")
+    # return find_min
+
+   
+    if weather_data == []: #if list is empty
+        return() #skip
+    else: #if list has content
+        min_val = float(weather_data[0]) #min value = typecasted first index of list
+        for i in range(0,len(weather_data),1): #for weather temp in range to length of list
+            if min_val >= float(weather_data[i]): #if min value greater than index
+                min_val = min(min_val, float(weather_data[i])) #min function used to declare the min value in the list
+                min_index = i #min index equals index
+    return min_val, min_index
+    
+print(find_min(weather_data))
+    # if weather_data != []:
+    #     for element in weather_data:
+    #         element = float(element)
+    #         min_val = weather_data[0]
+    #         if element <= float(min_val):
+    #             min_val = element
+    #             min_loc = index
+    #         index += 1
+    # return min_val, min_loc
+
+
 
 
 def find_max(weather_data):
@@ -101,8 +147,17 @@ def find_max(weather_data):
     Returns:
         The maximum value and it's position in the list.
     """
-    pass
+    if weather_data == []: #if list is empty
+        return() #skip
+    else: #if list has content
+        max_val = float(weather_data[0]) #min value = typecasted first index of list
+        for i in range(0,len(weather_data),1): #for weather temp in range to length of list
+            if max_val <= float(weather_data[i]): #if min value greater than index
+                max_val = max(max_val, float(weather_data[i])) #max function used to declare the max value in the list
+                max_index = i #min index equals index
+    return max_val, max_index
 
+print(find_max(weather_data))
 
 def generate_summary(weather_data):
     """Outputs a summary for the given weather data.
